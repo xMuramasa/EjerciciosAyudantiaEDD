62e63b73e272afd79e307325c23a0c41a21fd1ba
Martin Salinas Scussolin <martin.salinas.scussolin@gmail.com>
2020-04-13 20:35:26 -0400

    up ayudantia

---
+++ Ayudantia1.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+
+/* 
+* Problema 1
+*
+* Dado que nos gusta la geometría... xdxdxdxxxdx joajoajoajoa
+* Escribir un programa que reciba dos catetos de un tríangulo (floats) 
+* y calcule el valor de la hipotenusa. 
+* Nota: no se aceptan largos inválidos y su programa debe avisar.
+*
+*/
+
+/*
+* Problema 2
+* 
+* Leer 3 números de punto flotante. Luego, imprimir las raíces usando 
+* la formula para ecuaciones cuadráticas(Bhaskara) ax^2 + bx + c.
+* Nota: Si es imposible calcular las raíces, imprimir “Imposible de calcular”.
+*
+*/
+
+/*
+* Problema 3
+*
+* Realice un programa que reciba coordenadas X e Y y determina en que cuadrante pertenece el punto (X,Y).
+* Si el punto está en algún eje, escribir Eje Y/Eje X. Si el punto está en el origen, imprimir “Origen".
+*
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

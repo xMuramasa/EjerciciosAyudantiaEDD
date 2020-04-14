237fb9a204bfcc1c352dde625f4d371caa87ae9c
Martin Salinas Scussolin <martin.salinas.scussolin@gmail.com>
2020-04-13 21:50:48 -0400

    ayudantia1edd (ejercicios)

--- Ayudantia1.c
+++ Ayudantia1.c
@@ -1,34 +1,91 @@
+// LEARN GITHUB
+
 #include "stdio.h"
 
+// Notas rapidas
+
+// Para compilar un programa en C:
+/*
+    gcc -o nombreArchivoSalida nombrePrograma.c
+*/
+
+// Para ejecutar en linux
+/*
+    ./nombreArchivoSalida
+*/
+
+// *-----------------------------------------> Calentamiento previo
+// Problema 0.1
+/*
+    Realice un programa que reciba por pantalla el año de nacimiento 
+    de alguien y muestre en pantalla su edad.
+*/
+
+// Problema 0.2
+/*
+    Realice un programa que reciba 3 años de nacimiento y muestre en pantalla:
+       - Las 3 edades
+       - La suma de las 3 edades
+       - El promedio de los 3 años de nacimiento
+*/
+// *-----------------------------------------> Fin Calentamiento previo
+
+//  Problema 1
 /* 
-* Problema 1
-*
-* Dado que nos gusta la geometría... xdxdxdxxxdx joajoajoajoa
-* Escribir un programa que reciba dos catetos de un tríangulo (floats) 
-* y calcule el valor de la hipotenusa. 
-* Nota: no se aceptan largos inválidos y su programa debe avisar.
-*
+     Dado que nos gusta la geometría... xdxdxdxxxdx joajoajoajoa
+     Escribir un programa que reciba dos catetos de un tríangulo (floats) 
+     y calcule el valor de la hipotenusa. 
+     Nota: no se aceptan largos inválidos y su programa debe avisar.
+
+       Ingrese catetos: 0.0 20.0 
+       Hipotenusa: Imposible de calcular.
+
+       Ingrese catetos: 10.3 203.0
+       Hipotenusa: 203.261137456
+
+       Ingrese catetos: 10.3 203.0
+       Hipotenusa: Imposible de calcular.
 */
 
+// Problema 2
+/*   
+     Leer 3 números de punto flotante. Luego, imprimir las raíces usando 
+     la formula para ecuaciones cuadráticas(Bhaskara) ax^2 + bx + c.
+     Nota: Si es imposible calcular las raíces, imprimir “Imposible de calcular”.
+
+        Ingrese catetos: 0.0 20.0 \\
+        Hipotenusa: Imposible de calcular.
+        \vspace{5}
+
+        Ingrese catetos: 10.3 203.0\\
+        Hipotenusa: 203.261137456
+
+        Ingrese catetos: 10.3 203.0\\
+        Hipotenusa: Imposible de calcular.
+
+*/
+
+// Problema 3
 /*
-* Problema 2
-* 
-* Leer 3 números de punto flotante. Luego, imprimir las raíces usando 
-* la formula para ecuaciones cuadráticas(Bhaskara) ax^2 + bx + c.
-* Nota: Si es imposible calcular las raíces, imprimir “Imposible de calcular”.
-*
+     Realice un programa que reciba coordenadas X e Y y determina en que cuadrante pertenece el punto (X,Y).
+     Si el punto está en algún eje, escribir Eje Y/Eje X. Si el punto está en el origen, imprimir “Origen".
+
+       Input: 4.5 -2.2
+       Output: Cuadrante 4
+    
+       Input: 0.0 1.5
+       Output: Eje Y
 */
 
-/*
-* Problema 3
-*
-* Realice un programa que reciba coordenadas X e Y y determina en que cuadrante pertenece el punto (X,Y).
-* Si el punto está en algún eje, escribir Eje Y/Eje X. Si el punto está en el origen, imprimir “Origen".
-*
-*/
-
+// Main function such wao
 int main(int argc, char const *argv[])
 {
-    /* code */
+    /* Your c code goes here */
+    
+    // hola mundo
+    printf("Henlo\n");
     return 0;
 }
+
+
+// Learn GITHUB
